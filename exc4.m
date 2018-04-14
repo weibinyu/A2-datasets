2e1ea74240a53ed8f99f481a345a604db4ce3740
weibinyu <a1994218@hotmail.com>
2018-04-15 01:35:34 +0800

    4.14

--- exc4.m
+++ exc4.m
@@ -2,6 +2,7 @@ a2.clear()
 load('football.mat')
 
 %% plot and test sigmoid
+
 %plotFootball(football_X, football_y)
 s = a2.sigmoid([0 1;2 3]);
 i = 0;
@@ -17,14 +18,12 @@ end
 %% calculation cost when B = 0
 beta = [0;0];
 cost = calculateC(football_X,football_y,beta);
+[beta,itera] = findB(football_X,football_y,0.0001);
 
-<<<<<<< HEAD
 %% cost function
 function cost = calculateC(X,y,B)
 cost = ((-1)/size(X,1))*((y.')*log(a2.sigmoid(X*B))+((1-y).')*log(1-a2.sigmoid(X*B)));
-=======
-[beta,itera] = findB(football_X,football_y,0.0001)
-
+end
 
 %% function finding B
 function [beta,itera] = findB(X,y,a)
@@ -41,9 +40,9 @@ beta = [0;0];
 itera = 0;
 while true
     itera = itera+1;
-    cost =((-1)/size(XX,1))*((y.')*log(a2.sigmoid(XX*beta))+((1-y).')*log(1-a2.sigmoid(XX*beta)));
+    cost = calculateC(XX,y,beta);
     next_beta = beta-(a*(XX.')*(a2.sigmoid(XX*beta)-y));
-    new_cost = ((-1)/size(XX,1))*((y.')*log(a2.sigmoid(XX*next_beta))+((1-y).')*log(1-a2.sigmoid(XX*next_beta)));
+    new_cost = calculateC(XX,y,next_beta);
     beta = next_beta;
     if cost > new_cost
         cost = new_cost;
@@ -53,5 +52,4 @@ while true
 end
 
 beta = beta;
->>>>>>> origin/master
 end
