c516a585ee592664b6351b06c6d65e23b922e759
weibinyu <a1994218@hotmail.com>
2018-04-15 01:29:06 +0800

    4.14

--- exc4.m
+++ exc4.m
@@ -7,14 +7,18 @@ s = a2.sigmoid([0 1;2 3]);
 i = 0;
 while true
     if (a2.sigmoid(i)-1) == 0
-        disp(i);
+        disp("Matlab interprets sigmoid("+i+")-1 as zero");
         break
     end
     i = i+1;
 end
-% Matlab interprets sigmoid(37)-1 as zero
 
-%% calculation cost
+
+%% calculation cost when B = 0
 beta = [0;0];
-jb=((-1)/size(football_X,1))*((football_y.')*log(a2.sigmoid(football_X*beta))+((1-football_y).')*log(1-a2.sigmoid(football_X*beta)));
+cost = calculateC(football_X,football_y,beta);
 
+%% cost function
+function cost = calculateC(X,y,B)
+cost = ((-1)/size(X,1))*((y.')*log(a2.sigmoid(X*B))+((1-y).')*log(1-a2.sigmoid(X*B)));
+end
