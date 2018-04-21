f96d5f6352709b50a45a752c8fed7538d0ccb1f5
weibinyu <a1994218@hotmail.com>
2018-04-21 18:18:54 +0800

    4.21

--- exc8.m
+++ exc8.m
@@ -5,11 +5,11 @@ X = GPUbenchmark(:,1:6);
 y = GPUbenchmark(:,7);
 
 %%coefficients is Beta
-model = fs(X,y);
-kfcv(X,y,3,model)
+[model,selectedF] = fs(X,y);
+kfcv(X,y,3,model,selectedF)
 
 %% k-fold cross-validation
-function cVali = kfcv(X,y,k,model)
+function cVali = kfcv(X,y,k,model,selectedF)
 n = length(y);
 r = randperm(length(y)); % permutation of the vector 1:n
 f = floor(n/k); % largest integer ? n/k
@@ -21,23 +21,26 @@ for i=1:k
 idx = setdiff(cat(1,rp{:}),rp{i});
 tmpX = X(idx,:);
 tmpy = y(idx);
-tmpX = [ones(size(tmpy)),tmpX];
+temp = ones(size(tmpy));
 length(model)
 pre = [];
 for i = 1:length(model)
     cM = model{i};
     B = cM.Coefficients.Estimate;
+    temp = [temp, tmpX(:,selectedF(i))];
+    tmpX(:,selectedF(i))=[];
 end
 end
 cVali = 0;
 end
 
 %% forward selection
-function f = fs(X,y)
+function [f,selectedF] = fs(X,y)
 p = size(X,2)+1;
 XX = ones(size(y));
 mdl = fitlm(XX,y);
 Model{1} = mdl;
+selectedF=[];
 for k=1:p-1
     mse = Model{1}.MSE;
     n=1;
@@ -50,9 +53,11 @@ for k=1:p-1
         end
     end
     Model{k+1}= best;
+    selectedF = [selectedF,n]
     XX = [XX,X(:,n)];
     X(:,n)=[];
 end
+
 f = Model;
 end
 
