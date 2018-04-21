29e704f4c8684159ab68fa0acf1fe7f543198ec2
weibinyu <a1994218@hotmail.com>
2018-04-21 17:45:13 +0800

    4.21

--- exc8.m
+++ exc8.m
@@ -3,10 +3,13 @@ load('GPUbenchmark.csv')
 
 X = GPUbenchmark(:,1:6);
 y = GPUbenchmark(:,7);
-kfcv(X,y,2)
+
+%%coefficients is Beta
+model = fs(X,y);
+kfcv(X,y,3,model)
 
 %% k-fold cross-validation
-function cVali = kfcv(X,y,k)
+function cVali = kfcv(X,y,k,model)
 n = length(y);
 r = randperm(length(y)); % permutation of the vector 1:n
 f = floor(n/k); % largest integer ? n/k
@@ -18,11 +21,48 @@ for i=1:k
 idx = setdiff(cat(1,rp{:}),rp{i});
 tmpX = X(idx,:);
 tmpy = y(idx);
-
-%train model on tmpX, tmpy
-
+tmpX = [ones(size(tmpy)),tmpX];
+length(model)
+pre = [];
+for i = 1:length(model)
+    cM = model{i};
+    B = cM.Coefficients.Estimate;
+end
 end
 cVali = 0;
 end
 
 %% forward selection
+function f = fs(X,y)
+p = size(X,2)+1;
+XX = ones(size(y));
+mdl = fitlm(XX,y);
+Model{1} = mdl;
+for k=1:p-1
+    mse = Model{1}.MSE;
+    n=1;
+    for i=1:p-k
+        new_mdl = fitlm([XX,X(:,i)],y);
+        if new_mdl.MSE < mse
+            mse = new_mdl.MSE;
+            best = new_mdl;
+            n = i;
+        end
+    end
+    Model{k+1}= best;
+    XX = [XX,X(:,n)];
+    X(:,n)=[];
+end
+f = Model;
+end
+
+
+
+
+
+
+
+
+
+
+
